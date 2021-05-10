c3b4833f52c32449e07fb9ac6239f78882b975a6
Carson Rottinghaus <cqrottinghaus@gmail.com>
2021-05-09 21:37:59 -0500

    Improved modularity by adding Procedures

--- RestaurantDB.sql
+++ RestaurantDB.sql
@@ -59,6 +59,51 @@ CREATE TABLE ORDER_ITEM (
 	CONSTRAINT orderItem PRIMARY KEY (orderID, itemName)
 );
 
+-- PROCEDURES
+DELIMITER // -- UPDATES ORDER_DATA.totalCost attribute given specific orderID
+CREATE DEFINER=root@localhost PROCEDURE calculateOrderTotal (IN orderID INT)
+BEGIN
+UPDATE ORDER_DATA 
+SET ORDER_DATA.totalCost = (SELECT	SUM(i.price)
+							FROM	inventory i,order_item s
+							WHERE	i.itemName = s.itemName and s.orderID = orderID
+							)
+WHERE ORDER_DATA.orderID = orderID;
+END//
+DELIMITER ;
+
+DELIMITER // -- UPDATES REWARDS_MEMBER.rewardsPoints based on their order total * 10 
+CREATE DEFINER=root@localhost PROCEDURE addRewardsPoints(IN customerID INT)
+BEGIN
+UPDATE REWARDS_MEMBER m, ORDER_DATA d
+SET m.rewardsPoints = d.totalCost * 10
+WHERE m.customerID = customerID and d.customerID = customerID;
+END//
+DELIMITER ;
+
+DELIMITER // -- UPDATES DAILY_STATS.dailyIncome attribute given specifc orderDate
+CREATE DEFINER=root@localhost PROCEDURE calculateDailyIncome(IN orderDate DATE)
+BEGIN
+UPDATE DAILY_STATS
+SET DAILY_STATS.dailyIncome = (SELECT	SUM(d.totalCost)
+							FROM	order_data d 
+							WHERE d.orderDate = orderDate
+							);
+END//
+DELIMITER ;
+
+DELIMITER // -- UPDATES DAILY_STATS.dailyOrderAmt attribute given a specific orderDate
+CREATE DEFINER=root@localhost PROCEDURE calculateDailyOrderAmt(IN orderDate DATE)
+BEGIN
+UPDATE DAILY_STATS s
+SET s.dailyOrderAmt = (SELECT COUNT(*)
+					  FROM ORDER_DATA d
+					  WHERE d.orderDate = orderDate
+						)
+WHERE s.orderDate = orderDate;
+END//
+DELIMITER ;
+
 -- INSERTION
 INSERT INTO RESTURAUNT VALUES ();
 INSERT INTO DAILY_STATS (orderDate) VALUES ('2021-05-07');
@@ -73,50 +118,23 @@ INSERT INTO REWARDS_MEMBER (customerID, Fname, Lname, Address) VALUES (1, 'Carso
 INSERT INTO CUSTOMER VALUES ();
 INSERT INTO REWARDS_MEMBER (customerID, Fname, Lname, Address) VALUES (2, 'John', 'Doe','1600 Address Street');
 
+-- EXAMPLE USAGE
 -- Order Ticket 1
 INSERT INTO ORDER_DATA (customerID, totalCost, orderDate) VALUES(1,0,'2021-05-07');
 INSERT INTO ORDER_ITEM VALUES(1,'Pizza'), (1,'Fries'), (1,'Sandwich');
-
-UPDATE ORDER_DATA -- UPDATES ORDER_DATA.totalCost attribute given specific orderID
-SET ORDER_DATA.totalCost = (SELECT	SUM(i.price)
-							FROM	inventory i, order_item s
-							WHERE	i.itemName = s.itemName and s.orderID = 1
-							)
-WHERE ORDER_DATA.OrderID = 1;
-
-UPDATE REWARDS_MEMBER m, ORDER_DATA d -- UPDATES REWARDS_MEMBER.rewardsPoints based on their order total * 10 
-SET m.rewardsPoints = d.totalCost * 10
-WHERE m.customerID = 1 and d.customerID = 1;
+CALL calculateOrderTotal(1);
+CALL addRewardsPoints(1);
 
 -- Order Ticket 2
 INSERT INTO ORDER_DATA (customerID, totalCost, orderDate) VALUES(2,0,'2021-05-07');
 INSERT INTO ORDER_ITEM VALUES(2,'Chicken'), (2,'Fries'), (2,'Sandwich');
-
-UPDATE ORDER_DATA -- UPDATES ORDER_DATA.totalCost attribute given specific orderID
-SET ORDER_DATA.totalCost = (SELECT	SUM(i.price)
-							FROM	inventory i, order_item s
-							WHERE	i.itemName = s.itemName and s.orderID = 2
-							)
-WHERE ORDER_DATA.OrderID = 2;
-
-UPDATE REWARDS_MEMBER m, ORDER_DATA d -- QUERY UPDATES REWARDS_MEMBER.rewardsPoints based on their order total * 10 
-SET m.rewardsPoints = d.totalCost * 10
-WHERE m.customerID = 2 and d.customerID = 2;
+CALL calculateOrderTotal(2);
+CALL addRewardsPoints(2);
 
 
 -- DAILY_STATS UPDATE QUERIES
-UPDATE DAILY_STATS -- UPDATES DAILY_STATS.dailyIncome attribute given specifc orderDate
-SET DAILY_STATS.dailyIncome = (SELECT	SUM(d.totalCost)
-							FROM	order_data d 
-							WHERE d.orderDate = '2021-05-07'
-							);
-
-UPDATE DAILY_STATS s -- UPDATES DAILY_STATS.dailyOrderAmt attribute given a specific orderDate
-SET s.dailyOrderAmt = (SELECT COUNT(*)
-					  FROM ORDER_DATA d
-					  WHERE d.orderDate = '2021-05-07'
-						)
-WHERE s.orderDate = '2021-05-07';
+CALL calculateDailyIncome('2021-05-07');
+CALL calculateDailyOrderAmt('2021-05-07');
 
 
 -- MODIFICATION
